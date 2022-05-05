7ce96ff023f9a219ff93f877fe0eb2d8f094e565
Dani <nneodani@gmail.com>
2022-05-05 13:30:15 +0200

    done

--- script.js
+++ script.js
@@ -1,27 +1,99 @@
-const canvas = document.getElementById('shapes');
-const ctx = canvas.getContext('2d');
-console.log(ctx);
+ const canvas=document.getElementById("shapes")
+    const ctx=canvas.getContext('2d');  
 
-// Limpiar el lienzo
-ctx.clearRect(0, 0, 1000, 500) // x,y,width,height
+      ctx.clearRect(0, 0, 500, 500);
 
-// Cuadrado
-ctx.fillStyle = 'purple'; // Defino el color
-ctx.fillRect(260, 260, 30, 30) // x,y,width,height
+     const sun =new Image()
+     sun.src="./img/sun.png"
 
-// Rectángulo pero con borde
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 20;
-ctx.strokeRect(300, 300, 70, 70); // x,y,width,height
+     const barco =new Image()
+     barco.src="./img/barco.png"
 
-// Arc
-ctx.beginPath(); // Empiezo a dibujar
-ctx.lineWidth = 15; 
-ctx.fillStyle = 'yellow';
-ctx.strokeStyle = 'purple';
-ctx.arc(100, 75, 60, 0, 2 * Math.PI); // x centro, y centro, radius, angulo empieza, angulo que acaba
-ctx.fill(); // Rellénate
-ctx.stroke(); // Pinta tu borde
+     const tree =new Image()
+     tree.src="./img/tree.png" 
+     
+     const farola =new Image()
+     farola.src="./img/farola.png"
+
+     const star=new Image()
+     star.src="./img/star.png"
+
+     const valla=new Image()
+     valla.src="./img/valla.png"
+     let X=-350
+     let w;      
+        
+     setTimeout(()=>{
+      
+ setInterval(()=>{
+      
+            ctx.clearRect(0,0,1400,1400)
+            X++
+            X===1400?X=-350:null
+            ctx.drawImage(barco,X,320,400,200)               
+            ctx.drawImage(sun, -100, -100, 400, 400);      
+            ctx.drawImage(farola, 40, 250, 250, 250); 
+            positionTree.forEach( ele=>{     
+                ctx.drawImage(tree, ele.x, 250, 350, 350);      
+            })
+            w=Math.floor(Math.random(10)*10)
+            positionStar.forEach( ele=>{                 
+                ctx.drawImage(star, ele.x,ele.y, w, w);      
+             })
+            
+    ctx.drawImage(valla,0,400,1350,550)
+
+    },100)
+        
+   
+     },1000)
+   
+    const positionTree=[
+     {
+      x: 0,  
+     }, 
+    {
+        x: 350,            
+     },
+    {
+        x: 750,	        
+     },
+    {
+        x: 1050,  	  
+     }
+    ]    
+     const positionStar=[
+         {
+             x:100,
+                 y:5,
+                
+         },
+          {
+         x:250,
+             y:90
+         },
+          {
+             x:400,
+                 y:40
+         },
+          {
+            x:650,
+                y:90
+        },
+          {
+            x:850,
+                y:60
+         },
+          {
+            x:1050,
+                y:10
+         },
+          {
+            x:1250,
+                y:50
+         },
+
+     ]
 
 
 
